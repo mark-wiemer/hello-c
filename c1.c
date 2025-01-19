159a84be5aeff0c06223022cbdfeabdf5a7e4212
Mark Wiemer <markwiemer@outlook.com>
2025-01-18 22:17:38 -0800

    Add py_len func

--- c1.c
+++ c1.c
@@ -1,2 +1,15 @@
 // Chapter 1
 // https://youtu.be/PaPN51Mm5qQ?si=Hl_IKVrjAP863Tj3&t=6378
+#include <stdio.h>
+
+int py_len(char self[])
+{
+    int i;
+    for (i = 0; self[i]; i++);
+    return i;
+}
+
+int main() {
+    char x[] = "Hello";
+    printf("%s %d\n", x, py_len(x));
+}
