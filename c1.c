c1a0fd1664cbe723c34c5852c1dd7fad6d2e6d22
Mark Wiemer <markwiemer@outlook.com>
2025-01-18 21:06:57 -0800

    Move earlier functions earlier in the file

--- c1.c
+++ c1.c
@@ -5,15 +5,6 @@
 // View current default compiler settings with `gcc -v` and `echo | gcc -dM -E -x c - > out.txt`
 #include <stdio.h>
 
-/// Prints the first 100 characters of the user's name
-/// - Assumes user input is a single word
-int helloUser() {
-    char name[100];
-    printf("What's your first name? ");
-    scanf("%100s", name);
-    printf("Hello %s!\n", name);
-}
-
 /// Converts US floor number to European floor number.
 /// - Assumes user input is a number
 int usFloorToEuFloor() {
@@ -22,6 +13,15 @@ int usFloorToEuFloor() {
     scanf("%d", &usf);
     euf = usf - 1;
     printf("EU floor: %d\n", euf);
+}
+
+/// Prints the first 100 characters of the user's name
+/// - Assumes user input is a single word
+int helloUser() {
+    char name[100];
+    printf("What's your first name? ");
+    scanf("%100s", name);
+    printf("Hello %s!\n", name);
 }
 
 int main() {
