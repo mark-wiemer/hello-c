e42b8fc35f4ad039765cac740ed504598a00e869
Mark Wiemer <markwiemer@outlook.com>
2025-01-20 16:19:03 -0800

    Start chapter 3

--- c3.c
+++ c3.c
@@ -1,5 +1,10 @@
 // Chapter 2
 // https://youtu.be/PaPN51Mm5qQ?si=qhb2jguMfUrAEBau&t=13176
+// (no code)
+
+// Chapter 3
+// https://youtu.be/PaPN51Mm5qQ?si=Jpy8MGDwU8Mkt19I&t=18252
+
 
 #include <stdio.h>
 
