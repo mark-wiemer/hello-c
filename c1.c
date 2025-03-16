34c1de5f3aa98745a51e196a988f45f58603beca
Mark Wiemer <markwiemer@outlook.com>
2025-03-15 20:10:53 -0700

    ex 1.5

--- c1.c
+++ c1.c
@@ -64,7 +64,6 @@ int ex_1_3() {
 
 // Write a program to print the corresponding Celsius to Fahrenheit table.
 int ex_1_4() {
-
     /* print Celsius-Fahrenheit table
     for c = -20, -10, ..., 150 */
     int lower, upper, step;
@@ -82,6 +81,25 @@ int ex_1_4() {
     }
 }
 
+// Modify the temperature conversion program to print the table in reverse order, that is, from 300 degrees to 0.
+int ex_1_5() {
+    /* print Celsius-Fahrenheit table
+    for f = 300, 280, ... 0 */
+    int lower, upper, step;
+    float fahr, celsius;
+    lower = 0; /* lower limit of temperature table */
+    upper = 300; /* upper limit */
+    step = 20; /* step size */
+    fahr = upper;
+
+    printf("Fahr  Celsius\n");
+    while (fahr >= lower) {
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%4.0f   %6.1f\n", fahr, celsius);
+        fahr = fahr - step;
+    }
+}
+
 int main() {
-    ex_1_4();
+    ex_1_5();
 }
