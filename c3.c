9de976e3da9d98a99bb21a358cc350181d779d5a
Mark Wiemer <markwiemer@outlook.com>
2025-03-15 19:59:30 -0700

    Add link to c3 book

--- c3.c
+++ c3.c
@@ -3,8 +3,9 @@
 // (no code)
 
 // Chapter 3
-// https://youtu.be/PaPN51Mm5qQ?si=Jpy8MGDwU8Mkt19I&t=18252
-
+// Intro: https://youtu.be/PaPN51Mm5qQ?si=Jpy8MGDwU8Mkt19I&t=18252
+// Book:  https://youtu.be/PaPN51Mm5qQ?si=tAKNmbJVSPmFZ90_&t=18989
+//
 
 #include <stdio.h>
 
